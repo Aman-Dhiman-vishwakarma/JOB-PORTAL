806820b9db218a1417ff7aadcaa454b7bf48cb78
Aman-Dhiman-vishwakarma <168437901+Aman-Dhiman-vishwakarma@users.noreply.github.com>
2024-07-31 15:54:25 +0530

    Update constant.js

--- clint/src/utils/constant.js
+++ clint/src/utils/constant.js
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT = "https://job-portal-backend-xgyk.onrender.com/api/v1/user";
-export const JOB_API_END_POINT = "https://job-portal-backend-xgyk.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT = "https://job-portal-backend-xgyk.onrender.com/api/v1/applications";
-export const COMPANY_API_END_POINT = "https://job-portal-backend-xgyk.onrender.com/api/v1/company";
+export const USER_API_END_POINT = "https://job-portal-backend-i7v7.onrender.com/api/v1/user";
+export const JOB_API_END_POINT = "https://job-portal-backend-i7v7.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT = "https://job-portal-backend-i7v7.onrender.com/api/v1/applications";
+export const COMPANY_API_END_POINT = "https://job-portal-backend-i7v7.onrender.com/api/v1/company";
 
