49d70e966387a25b14feadbff0064bd2d9d43acc
Aman-Dhiman-vishwakarma <168437901+Aman-Dhiman-vishwakarma@users.noreply.github.com>
2024-07-31 15:44:46 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,11 +14,8 @@ dotenv.config({})
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
-const corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true,
-};
-app.use(cors(corsOptions));
+
+app.use(cors());
 
 //api's
 app.use("/api/v1/user", userRoute)
