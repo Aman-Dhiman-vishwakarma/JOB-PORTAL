e08b162a58273fe77e3f2d3936669d6f9c7d1276
Aman-Dhiman-vishwakarma <168437901+Aman-Dhiman-vishwakarma@users.noreply.github.com>
2024-08-01 10:50:20 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -94,7 +94,7 @@ export const login = async (req, res) => {
 
     return res
       .status(200)
-      .cookie("token", token, { htttpsOnly: true, sameSite: "strict" })
+      .cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: "strict" })
       .json({
         message: `Welcome back ${user.fullname}`,
         user: {
